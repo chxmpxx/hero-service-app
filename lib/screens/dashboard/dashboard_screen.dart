6416c0d73e14c8311584953884db0b3a991e6761
Arreeya <arreeya.su@ku.th>
2022-08-19 20:56:26 +0700

    update bottomnav: curvednav

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
 import 'package:hero_service_app/screens/bottomnav/booking_screen.dart';
 import 'package:hero_service_app/screens/bottomnav/home_screen.dart';
 import 'package:hero_service_app/screens/bottomnav/market_screen.dart';
+import 'package:hero_service_app/screens/bottomnav/notification_screen.dart';
 import 'package:hero_service_app/screens/bottomnav/setting_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -14,13 +17,14 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
 
   // สร้างตัวแปรแบบ List ไว้เก็บรายการของ bottom nav
-  int _currentIndex = 0;
+  int _currentIndex = 2;
   String _title = 'บริการ';
 
   final List<Widget> _children = [
-    HomeScreen(),
     MarketScreen(),
     BookingScreen(),
+    HomeScreen(),
+    NotificationScreen(),
     SettingScreen()
   ];
 
@@ -32,13 +36,15 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
       // เปลี่ยน title ตาม teb ที่เลือก
       switch (index) {
-        case 0: _title = 'บริการ';
+        case 0: _title = 'ตลาด';
           break;
-        case 1: _title = 'ตลาด';
+        case 1: _title = 'รายการจอง';
           break;
-        case 2: _title = 'รายการจอง';
+        case 2: _title = 'บริการ';
           break;
-        case 3: _title = 'อื่น ๆ';
+        case 3: _title = 'แจ้งเตือน';
+          break;
+        case 4: _title = 'อื่น ๆ';
           break;
       }
     });
@@ -51,20 +57,43 @@ class _DashboardScreenState extends State<DashboardScreen> {
         title: Center(child: Text(_title)),
         backgroundColor: Colors.pink[400],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        // ถ้ามีพารามิเตอร์เดียวไม่ต้องใส่ก็ได้
+      // bottomNavigationBar: BottomNavigationBar(
+      //   // ถ้ามีพารามิเตอร์เดียวไม่ต้องใส่ก็ได้
+      //   onTap: onTabTapped,
+      //   // อ่านตำแหน่งปัจจุบันที่กด
+      //   currentIndex: _currentIndex,
+      //   backgroundColor: Colors.pink[400],
+      //   selectedItemColor: Colors.white,
+      //   unselectedItemColor: Colors.grey[300],
+      //   type: BottomNavigationBarType.fixed,
+        // items: [
+        //   BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'ตลาด'),
+        //   BottomNavigationBarItem(icon: Icon(Icons.library_add_sharp), label: 'รายการจอง'),
+        //   BottomNavigationBarItem(icon: Icon(Icons.business_center), label: 'บริการ'),
+        //   BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'แจ้งเตือน'),
+        //   BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'อื่น ๆ'),
+        // ],
+      // ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 2,
         onTap: onTabTapped,
-        // อ่านตำแหน่งปัจจุบันที่กด
-        currentIndex: _currentIndex,
-        backgroundColor: Colors.pink[400],
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey[300],
-        type: BottomNavigationBarType.fixed,
+        // สีพื้นหลัง
+        backgroundColor: Colors.white,
+        // สีเมนู
+        color: Colors.pink,
+        buttonBackgroundColor: Colors.pink,
+        height: 60,
+        animationDuration: Duration(
+          // 0.2 วิ
+          milliseconds: 200
+        ),
+        animationCurve: Curves.bounceInOut,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.business_center), label: 'บริการ'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'ตลาด'),
-          BottomNavigationBarItem(icon: Icon(Icons.library_add_sharp), label: 'รายการจอง'),
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'อื่น ๆ'),
+          Icon(Icons.shopping_cart, size: 30, color: Colors.white,),
+          Icon(Icons.library_add_sharp, size: 30, color: Colors.white,),
+          Icon(Icons.business_center, size: 30, color: Colors.white,),
+          Icon(Icons.notifications, size: 30, color: Colors.white,),
+          Icon(Icons.menu, size: 30, color: Colors.white,)
         ],
       ),
       body: _children[_currentIndex]
